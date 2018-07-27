b42a8b8e6a822b6075fe98d6f673d166acfc6ca5
Jakub Gucwa <jakub.gucwa@outlook.com>
2018-07-28 00:29:18 +0200

    Modified code, added class for calculations, modified code of Program class.

--- GitCalc/GitCalc/Program.cs
+++ GitCalc/GitCalc/Program.cs
@@ -3,21 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GitCalc.Calculations;
 
 namespace GitCalc
 {
     class Program
     {
-        private static int a;
+        private static double returned = 0;
+        private static bool flag = false;
         static void Main(string[] args)
         {
+            double a;
+            double b;
+            string dzial;
+
+            flag = false;
+
+           // do
+           // {
+                Console.WriteLine("Hello, please insert first number");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("insert second number");
+                b = double.Parse(Console.ReadLine());
+            //} while (flag);
 
 
 
-            Console.WriteLine("Hello please insert number");
-            a = Int32.Parse(Console.ReadLine());
-            a = a * 4;
-            Console.WriteLine("your number multiplied by 4 equals:\r\n {0}", a.ToString());
+            Console.WriteLine("select equation: \n 1. Addition\n 2. Minus\n 3. Multiplication ");
+
+            dzial = Console.ReadLine();
+
+            if (dzial.Equals("1"))
+            {
+                returned = Calculations.Add(a, b);
+            }
+
+             else if (dzial.Equals("2"))
+            {
+                returned = Calculations.Minus(a, b);
+            }
+
+            else if (dzial.Equals("3"))
+            {
+                returned = Calculations.Times(a, b);
+            }
+
+            else
+            {
+                returned = -99.99;
+                Console.WriteLine("not valid equation");
+            }
+
+            Console.WriteLine("here is your calculated value");
+            Console.WriteLine(returned.ToString());
             Console.ReadKey();
         }
     }
---
+++ GitCalc/GitCalc/calculations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCalc
+{
+    class Calculations
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Times(double a, double b)
+        {
+            return a * b;
+        }
+    }
+
+}
